8bcb2e41f1807a83866a0f942e2342d52a335b13
Ettienne Gous <egous@ie.com.au>
2019-07-29 14:28:18 +1000

    Add custom condition and profile viewer sample code

---
+++ MyAssembly/MyAssembly/ProfileCompareToProfileCondition.cs
@@ -0,0 +1,95 @@
+﻿using Sitecore.Analytics;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Rules;
+using Sitecore.Rules.Conditions;
+using System;
+
+namespace MyAssembly
+{
+    public class ProfileCompareToProfileCondition<T> : OperatorCondition<T> where T : RuleContext
+    {
+        private string profileKeyId;
+        private string profileKeyId2;
+
+        public string ProfileKeyId
+        {
+            get
+            {
+                return profileKeyId ?? string.Empty;
+            }
+            set
+            {
+                Assert.ArgumentNotNull(value, "value");
+                profileKeyId = value;
+            }
+        }
+
+        public string ProfileKeyId2
+        {
+            get
+            {
+                return profileKeyId2 ?? string.Empty;
+            }
+            set
+            {
+                Assert.ArgumentNotNull(value, "value");
+                profileKeyId2 = value;
+            }
+        }
+
+        public ProfileCompareToProfileCondition()
+        {
+            profileKeyId = string.Empty;
+            profileKeyId2 = string.Empty;
+        }
+
+        protected override bool Execute(T ruleContext)
+        {
+            Assert.ArgumentNotNull(ruleContext, "ruleContext");
+            double profileKeyValue1 = GetProfileKeyValue(ProfileKeyId);
+            double profileKeyValue2 = GetProfileKeyValue(ProfileKeyId2);
+            switch (GetOperator())
+            {
+                case ConditionOperator.Equal:
+                    return Math.Abs(profileKeyValue1 - profileKeyValue2) < 0.001;
+                case ConditionOperator.GreaterThanOrEqual: return profileKeyValue1 >= profileKeyValue2;
+                case ConditionOperator.GreaterThan:
+                    return profileKeyValue1 > profileKeyValue2;
+                case ConditionOperator.LessThanOrEqual:
+                    return profileKeyValue1 <= profileKeyValue2;
+                case ConditionOperator.LessThan:
+                    return profileKeyValue1 < profileKeyValue2;
+                case ConditionOperator.NotEqual: return Math.Abs(profileKeyValue1 - profileKeyValue2) > 0.001;
+                default:
+                    return false;
+            }
+        }
+
+        private double GetProfileKeyValue(string profileKey)
+        {
+            double profileKeyScore = 0.0;
+            if (!string.IsNullOrEmpty(profileKey))
+            {
+                Item obj = Tracker.DefinitionDatabase.GetItem(profileKey);
+                if (obj != null)
+                {
+                    Item parent = obj.Parent;
+                    if (parent != null)
+                    {
+                        string profileKeyName = obj.Name;
+                        string profileName = parent.Name;
+
+                        var profileDetails = Tracker.Current.Interaction.Profiles[profileName];
+                        if (profileDetails != null)
+                        {
+                            profileKeyScore = profileDetails[profileKeyName];
+                        }
+
+                    }
+                }
+            }
+            return profileKeyScore;
+        }
+    }
+}
---
+++ MyAssembly/WebApplication/Controllers/ProfileScoreViewer.cs
@@ -0,0 +1,56 @@
+﻿using Sitecore.Analytics;
+using Sitecore.Diagnostics;
+using Sitecore.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class ProfileScoreViewerController : SitecoreController
+    {
+        // GET: Brochures
+        private string cookieKey = "psv";
+
+        public override ActionResult Index()
+        {
+            Dictionary<string, string> collection = new Dictionary<string, string>();
+            bool cookieJustDestroyed = false;
+            if (!String.IsNullOrWhiteSpace(Request.QueryString[cookieKey]) && (Request.QueryString[cookieKey] == "false" || Request.QueryString[cookieKey] == "0"))
+            {
+                Response.Cookies[cookieKey].Expires = DateTime.Now.AddDays(-1);
+                cookieJustDestroyed = true;
+            }
+
+            if ((!String.IsNullOrWhiteSpace(Request.QueryString[cookieKey]) && (Request.QueryString[cookieKey] == "1" || Request.QueryString[cookieKey] == "true")) || (Request.Cookies[cookieKey] != null && Request.Cookies[cookieKey].Value == "true" && !cookieJustDestroyed))
+            {
+                var cookie = new HttpCookie(cookieKey, "true");
+                cookie.Expires = DateTime.Now.AddDays(1);
+                Response.SetCookie(cookie);
+
+                foreach (var profileName in Tracker.Current.Interaction.Profiles.GetProfileNames())
+                {
+                    var profile = Tracker.Current.Interaction.Profiles[profileName]; //If profile does not exist, it will create that one.
+
+                    foreach (KeyValuePair<string, float> keyValuePair in profile)
+                    {
+                        string key = String.Format("{0} - {1}", profileName, keyValuePair.Key);
+                        if (!collection.ContainsKey(key))
+                        {
+                            collection.Add(key, String.Format("{0}", keyValuePair.Value));
+                        }
+
+
+                    }
+
+
+                }
+            }
+
+
+            return View("~/Views/ProfileScoreViewer.cshtml", collection);
+        }
+    }
+}
